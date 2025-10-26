f6f5e2a54c8c5e83d20064cf9cb2d83bcb7e4718
AbhinavDShetty <abhinavdshetty@gmail.com>
2025-10-26 21:13:16 +0530

    Corrected Initial Commit
    Changed the whole project, Basic Django Structure introduced

--- backend/chemical_backend/chemical_backend/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path("api/", include("molecules.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/chemical_backend/molecules/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import generate_molecule, blender_command
-
-urlpatterns = [
-    path("blender-command/", blender_command, name="blender_command"),
-    path("generate/", generate_molecule),
-]
-
