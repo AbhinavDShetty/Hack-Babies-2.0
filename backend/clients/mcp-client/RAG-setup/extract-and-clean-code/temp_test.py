71ba70f78c831fd60f16b3f20f82351751ad77ac
Chinmay P Kulkarni <chinmaykulkarni2077@gmail.com>
2025-11-03 19:28:04 +0530

    Change File Structure
    Moved the RAG-Setup folder outside the clients folder and into backend folder

--- backend/clients/mcp-client/RAG-setup/extract-and-clean-code/temp_test.py
+++
@@ -1,7 +0,0 @@
-import json
-with open("cleaned_chemblender_functions.json", "r", encoding="utf-8") as f:
-    functions = json.load(f)
-
-for func in functions:
-    if "code" in func:
-        print(func["code"])
